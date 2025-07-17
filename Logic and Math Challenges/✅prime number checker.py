9a309831be8a7ff51deacc0b010a44fb1dea4aa3
knmensahdarkwa7 <annonymoussilence7@gmail.com>
2025-07-17 17:43:31 +0000

    Code Decides if a given number is a prime number or not

---
+++ Logic and Math Challenges/✅prime number checker.py
@@ -0,0 +1,6 @@
+number =int(input('Enter your number:'))
+if number / 1 == number and number/ number  == 1:
+    print(f'{number} is a prime number')
+else:
+    print(f'{number} is not a prime number')
+
