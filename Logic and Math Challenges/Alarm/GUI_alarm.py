c2f97b589bf2b4a16d5421932e2aead76d57e605
knmensahdarkwa7 <annonymoussilence7@gmail.com>
2025-07-30 18:57:16 +0000

    Implement countdown timer using CustomTkinter with UI transitions, fullscreen mode, and audible alert on completion

--- Logic and Math Challenges/Alarm/GUI_alarm.py
+++ Logic and Math Challenges/Alarm/GUI_alarm.py
@@ -13,35 +13,44 @@ def exe():
     button.pack_forget()
 
     # Make window fullscreen
-    window.attributes('-fullscreen', True)
+    #window.attributes('-fullscreen', True)
 
-    time_label.pack(expand=True, fill='both')
+    #time_label.pack(expand=True, fill='both')
 
-    for x in range(int(entry.get()), 0, -1):
-        seconds = x % 60
-        minutes = (x // 60) % 60
-        hours = (x // 3600)
-        current_time = f"{hours:02}:{minutes:02}:{seconds:02}"
-        time_label.configure(text=current_time, width=500)
+    if entry.get() == '':
+        time_label.configure(text='No time')
         window.update()
-        sleep(1)
 
-    frequency = 3000
-    duration = 800
-    time_label.configure(text="Time's up!")
+    else:
+        for x in range(int(entry.get()), 0, -1):
+            seconds = x % 60
+            minutes = (x // 60) % 60
+            hours = (x // 3600)
+            current_time = f"{hours:02}:{minutes:02}:{seconds:02}"
+            time_label.configure(text=current_time, width=500)
+            window.update()
+            sleep(1)
 
-    for _ in range(5):
-        Beep(frequency, duration)
-        sleep(0.1)
 
+        frequency = 2000
+        duration = 800
+        time_label.configure(text="Time's up!")
+
+
+
+        for _ in range(5):
+            Beep(frequency, duration)
+            sleep(0.1)
     # Restore default window size and widgets
     window.attributes('-fullscreen', False)
     window.geometry('420x200')
 
+
+
     label.pack()
     entry.pack()
     button.pack(pady=10)
-
+    time_label.pack_forget()
 
 window = CTk()
 window.geometry('420x200')
@@ -53,7 +62,7 @@ label.pack()
 entry = CTkEntry(window, width=150, placeholder_text='Enter time in seconds')
 entry.pack()
 
-time_label = CTkLabel(window, text='', font=('Consolas', 24),anchor='center')
+time_label = CTkLabel(window, text='', font=('Consolas', 24))
 time_label.pack(pady=10,expand=True,fill='both')
 
 button = CTkButton(window, text='Count down', command=exe)
